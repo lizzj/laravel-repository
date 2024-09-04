c038d42b8d07d7a205ef7421eef162f36fe24b59
lizzj <li_zzj@163.com>
2024-09-04 11:58:23 +0800

    fix: mino:controller

--- src/Morisawa/Repository/Generators/Commands/ControllerCommand.php
+++ src/Morisawa/Repository/Generators/Commands/ControllerCommand.php
@@ -55,59 +55,81 @@ class ControllerCommand extends Command
      *
      * @return void
      */
-    public function fire()
-    {
-        $case_name = \Illuminate\Support\Str::title($this->argument("name"));
-        foreach (explode("\\", $case_name) as $item) {
-            if (blank($item) || !preg_match('/^[A-Z]/', $item[0])) {
-                $this->error($case_name.' Invalid Namespace!');
-                return false;
-            }
-        }
-        try {
-            $choices = [
-                1 => 'CreateRequest And UpdateRequest',
-                2 => 'CreateRequest',
-                3 => 'UpdateRequest',
-                0 => 'Nothing'
-            ];
-            $request_option = $this->choice(
-                'What would you like to do?',
-                $choices,
-                0
-            );
-            $generateRequests = function ($case_name, $requests) {
-                foreach ($requests as $request) {
-                    (new RequestGenerator(['name' => $case_name.'/'.$request]))->run();
-                }
-            };
-            $action = match (array_search($request_option, $choices)) {
-                1 => function () use ($case_name, $generateRequests) {
-                    $generateRequests($case_name, ['Create', 'Update']);
-                    $this->info('CreateRequest and UpdateRequest created successfully.');
-                },
-                2 => function () use ($case_name, $generateRequests) {
-                    $generateRequests($case_name, ['Create']);
-                    $this->info('CreateRequest created successfully.');
-                },
-                3 => function () use ($case_name, $generateRequests) {
-                    $generateRequests($case_name, ['Update']);
-                    $this->info('UpdateRequest created successfully.');
-                },
-                default => function () {
-                    $this->info('No request created.');
-                },
-            };
-            call_user_func($action);
-            (new ControllerGenerator([
-                'name' => $case_name
-            ]))->run();
-            $this->info($this->type.' created successfully.');
-        } catch (FileAlreadyExistsException $e) {
-            $this->error($this->type.' already exists!');
+public function fire()
+{
+    // Normalize the case name
+    $case_name = \Illuminate\Support\Str::title($this->argument("name"));
+
+    // Validate the namespace
+    foreach (explode("\\", $case_name) as $item) {
+        if (blank($item) || !preg_match('/^[A-Z]/', $item[0])) {
+            $this->error($case_name . ' Invalid Namespace!');
             return false;
         }
     }
+
+    try {
+        // Define choices
+        $choices = [
+            0 => 'Nothing',
+            1 => 'CreateRequest And UpdateRequest',
+            2 => 'CreateRequest',
+            3 => 'UpdateRequest'
+        ];
+
+        // Get user choice
+        $request_option = $this->choice(
+            'What would you like to do?',
+            array_values($choices), // Use array_values to get the list of options
+            0
+        );
+
+        // Map choice to action
+        $actionKey = array_search($request_option, $choices);
+
+        $generateRequests = function ($case_name, $requests) {
+            foreach ($requests as $request) {
+                (new RequestGenerator(['name' => $case_name . '/' . $request]))->run();
+            }
+        };
+
+        $action = match ($actionKey) {
+            1 => function () use ($case_name, $generateRequests) {
+                $generateRequests($case_name, ['Create', 'Update']);
+                $this->info('CreateRequest and UpdateRequest created successfully.');
+            },
+            2 => function () use ($case_name, $generateRequests) {
+                $generateRequests($case_name, ['Create']);
+                $this->info('CreateRequest created successfully.');
+            },
+            3 => function () use ($case_name, $generateRequests) {
+                $generateRequests($case_name, ['Update']);
+                $this->info('UpdateRequest created successfully.');
+            },
+            0 => function () {
+                $this->info('No request created.');
+            },
+            default => function () {
+                $this->info('Invalid option selected.');
+            },
+        };
+
+        // Call the selected action
+        call_user_func($action);
+
+        // Generate controller
+        (new ControllerGenerator([
+            'name' => $case_name
+        ]))->run();
+
+        $this->info('Controller created successfully.');
+    } catch (FileAlreadyExistsException $e) {
+        $this->error('File already exists!');
+        return false;
+    }
+}
+
+
         /**
      * The array of command arguments.
      *
