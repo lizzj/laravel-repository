57170146fa2394e86d36e26d807a44ee5d702690
lizzj <li_zzj@163.com>
2026-08-29 17:19:17 +0800

    Normalize cache TTL and Lumen version parsing
    Laravel 8+ standardizes cache TTL in seconds, so repository cache now always returns seconds. This also fixes Lumen version comparison parsing by correctly matching multi-digit versions and preferring the Laravel Components version when present.

--- src/Morisawa/Repository/Traits/CacheableRepository.php
+++ src/Morisawa/Repository/Traits/CacheableRepository.php
@@ -171,10 +171,10 @@ trait CacheableRepository
     }
 
     /**
-     * Get cache time
+     * Get cache TTL in seconds.
      *
-     * Return minutes: version < 5.8
-     * Return seconds: version >= 5.8
+     * Laravel 5.8+ standardized cache TTL on seconds. As of this release,
+     * Laravel 8 is the minimum supported version, so we always return seconds.
      *
      * @return int
      */
@@ -182,14 +182,7 @@ trait CacheableRepository
     {
         $cacheMinutes = isset($this->cacheMinutes) ? $this->cacheMinutes : config('repository.cache.minutes', 30);
 
-        /**
-         * https://laravel.com/docs/5.8/upgrade#cache-ttl-in-seconds
-         */
-        if ($this->versionCompare($this->app->version(), '5.7.*', '>')) {
-            return $cacheMinutes * 60;
-        }
-
-        return $cacheMinutes;
+        return $cacheMinutes * 60;
     }
 
     /**
